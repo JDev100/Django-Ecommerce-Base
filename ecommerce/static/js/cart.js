b2cc87b85289b0b369564b17147d0323ec4500af
DaMan <jcazdev100@protonmail.com>
2022-05-09 14:46:10 -0700

    Working on update order functionality

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,41 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (let i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action: ', action)
+
+        console.log('USER: ', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not logged in')
+        } else {
+            updateUserOrder(productId, action)
+        }
+    });
+}
+
+/**   Sends productId and action as a JSON object to the 'update_item' view to update user order */
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data...')
+
+    var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data: ', data)
+            location.reload()
+
+        })
+}
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,16 +1,27 @@
 
 from django.shortcuts import render
+from django.http import JsonResponse, HttpRequest
 from .models import *
+import json
 # Create your views here.
 
 
 def store(request):
+    """
+    This view renders the main store page along with all
+    the products for sale. Users can add items to their cart in
+    this view
+    """
     products = Product.objects.all
     context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
 def cart(request):
+    """
+    This view renders the cart items in current order
+    along with the total prices
+    """
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(
@@ -24,6 +35,10 @@ def cart(request):
 
 
 def checkout(request):
+    """
+    This view renders the checkout page along with
+    forms to add payment and shipping info
+    """
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(
@@ -34,3 +49,35 @@ def checkout(request):
         items = []
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    """
+    This view updates the user order by accessing a json object
+    sent by request to Add or Subtract item from cart using productId
+    to access proper product from database
+    """
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action: ', action)
+    print('Product: ', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order,product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()            
+
+    return JsonResponse('Item added', safe = False)
